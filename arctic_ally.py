1fa2d8cbf3cd5b8dedcf520c71a212058570a991
Rok Benko <ksjeno@gmail.com>
2024-05-06 19:08:12 +0200

    Add Streamlit tabs

--- arctic_ally.py
+++ arctic_ally.py
@@ -22,3 +22,31 @@ if uploaded_file is not None:
 
 def analyze():
     st.text("Analyzing...")
+
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["Summary", "Todo", "Speakers", "Sentiment", "Translation", "Insights", "Integrations", "Follow-ups"])
+
+with tab1:
+   st.header("Summary")
+   st.text("Summary: ...")
+   st.text("Keywords: ...")
+
+with tab2:
+   st.header("Todo")
+
+with tab3:
+   st.header("Speakers")
+
+with tab4:
+   st.header("Sentiment")
+
+with tab5:
+   st.header("Translation")
+
+with tab6:
+   st.header("Insights")
+
+with tab7:
+   st.header("Integrations")
+
+with tab8:
+   st.header("Follow-ups")
